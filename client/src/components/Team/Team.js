5d66ca901a9991409de98ce2dcdfc0a5e5aa4979
Dipanshuraj <dipanshuraj1818@gmail.com>
2022-08-22 23:09:18 +0530

    Team_pageV1.0

---
+++ client/src/components/Team/Team.js
@@ -0,0 +1,190 @@
+import React from "react";
+import "./Team.css";
+import Avatar1 from "../../assets/Avatar/Avatar1.jpg";
+import github_icon from "../../assets/icon/icon-github.svg";
+import linkedIn_icon from "../../assets/icon/icon-linkedin.svg";
+
+
+const Team = () => {
+    return (
+        <>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <h1 className="text-center">Meet The Team</h1>
+                    </div>
+                    <div className="col-12 mt-3 about-para">
+                        <p>
+                            Meet Team description will be here in few line.......
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="main-box">
+                <div className="member-card">
+                    <div className="Avatar-box">
+                        <img className="display-img" src={Avatar1} alt="Profile Image" />
+
+                    </div>
+                    <div className="Avatar-name">
+                        <h3>Avatar Name</h3>
+
+                    </div>
+                    <div className="Avatar-role">
+                        <h4>Avatar Role</h4>
+
+                    </div>
+                    <div className="Avatar-social-links">
+                        <a href="#">
+                            <img className="Social_icon" src={linkedIn_icon} alt="Twitter Link" />
+                        </a>
+                        <a href="#">
+                            <img className="Social_icon" src={github_icon} alt="Telegram Link" />
+                        </a>
+
+                    </div>
+
+                </div>
+
+                <div className="member-card">
+                    <div className="Avatar">
+                        <img className="display-img" src={Avatar1} alt="Profile Image" />
+
+                    </div>
+                    <div className="Avatar-name">
+                        <h3>Avatar Name</h3>
+
+                    </div>
+                    <div className="Avatar-role">
+                        <h4>Avatar Role</h4>
+
+                    </div>
+                    <div className="Avatar-social-links">
+                        <a href="#">
+                            <img className="Social_icon" src={linkedIn_icon} alt="Twitter Link" />
+                        </a>
+                        <a href="#">
+                            <img className="Social_icon" src={github_icon} alt="Telegram Link" />
+                        </a>
+
+                    </div>
+
+                </div>
+
+                <div className="member-card">
+                    <div className="Avatar">
+                        <img className="display-img" src={Avatar1} alt="Profile Image" />
+
+                    </div>
+                    <div className="Avatar-name">
+                        <h3>Avatar Name</h3>
+
+                    </div>
+                    <div className="Avatar-role">
+                        <h4>Avatar Role</h4>
+
+                    </div>
+                    <div className="Avatar-social-links">
+                        <a href="#">
+                            <img className="Social_icon" src={linkedIn_icon} alt="Twitter Link" />
+                        </a>
+                        <a href="#">
+                            <img className="Social_icon" src={github_icon} alt="Telegram Link" />
+                        </a>
+
+                    </div>
+
+                </div>
+
+                <div className="member-card">
+                    <div className="Avatar">
+                        <img className="display-img" src={Avatar1} alt="Profile Image" />
+
+                    </div>
+                    <div className="Avatar-name">
+                        <h3>Avatar Name</h3>
+
+                    </div>
+                    <div className="Avatar-role">
+                        <h4>Avatar Role</h4>
+
+                    </div>
+                    <div className="Avatar-social-links">
+                        <a href="#">
+                            <img className="Social_icon" src={linkedIn_icon} alt="Twitter Link" />
+                        </a>
+                        <a href="#">
+                            <img className="Social_icon" src={github_icon} alt="Telegram Link" />
+                        </a>
+
+                    </div>
+
+                </div>
+
+                <div className="member-card">
+                    <div className="Avatar">
+                        <img className="display-img" src={Avatar1} alt="Profile Image" />
+
+                    </div>
+                    <div className="Avatar-name">
+                        <h3>Avatar Name</h3>
+
+                    </div>
+                    <div className="Avatar-role">
+                        <h4>Avatar Role</h4>
+
+                    </div>
+                    <div className="Avatar-social-links">
+                        <a href="#">
+                            <img className="Social_icon" src={linkedIn_icon} alt="Twitter Link" />
+                        </a>
+                        <a href="#">
+                            <img className="Social_icon" src={github_icon} alt="Telegram Link" />
+                        </a>
+
+                    </div>
+
+                </div>
+
+                <div className="member-card">
+                    <div className="Avatar">
+                        <img className="display-img" src={Avatar1} alt="Profile Image" />
+
+                    </div>
+                    <div className="Avatar-name">
+                        <h3>Avatar Name</h3>
+
+                    </div>
+                    <div className="Avatar-role">
+                        <h4>Avatar Role</h4>
+
+                    </div>
+                    <div className="Avatar-social-links">
+                        <a href="#">
+                            <img className="Social_icon" src={linkedIn_icon} alt="Twitter Link" />
+                        </a>
+                        <a href="#">
+                            <img className="Social_icon" src={github_icon} alt="Telegram Link" />
+                        </a>
+
+                    </div>
+
+                </div>
+
+
+
+
+
+
+
+
+
+            </div>
+        </>
+
+
+    );
+};
+
+export default Team;
+
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -3,12 +3,14 @@ import TopBanner from "../../components/TopBanner/TopBanner";
 import About from "../../components/About/About";
 import Partners from "../../components/Partners/Partners";
 import Footer from "../../components/Footer/Footer";
+import Team from "../../components/Team/Team";
 const Home = () => {
   return (
     <div>
       <TopBanner />
       <About />
       <Partners />
+      <Team />
       <Footer />
     </div>
   );
